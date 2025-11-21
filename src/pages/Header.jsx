4f9fbdd4c77aef72eaf8cf450193eb55e50bccea
Andrey Baluhatyy <anbaluxes@gmail.com>
2025-11-21 21:13:32 +0300

    test-last-mb src

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({ shop, setSearch, search }) {
 		<header>
 			<div className='container'>
 				<div className='flex justify-between items-center'>
-					<img src='moon/header/logo.png' alt='Logo' className='w-44 h-12' />
+					<img src='header/logo.png' alt='Logo' className='w-44 h-12' />
 					<NavMenu />
 					<input
 						type='text'
@@ -15,7 +15,7 @@ export default function Header({ shop, setSearch, search }) {
 					/>
 					<div className='flex justify-between gap-5'>
 						<a href='#' className='w-6 h-6'>
-							<img src='moon/header/header_button/Heart.png' alt='Heart' />
+							<img src='header/header_button/Heart.png' alt='Heart' />
 						</a>
 						<span className='w-6 h-6 relative'>
 							{shop && (
@@ -25,13 +25,13 @@ export default function Header({ shop, setSearch, search }) {
 							)}
 							<a href='#'>
 								<img
-									src='moon/header/header_button/Shopping_cart.png'
+									src='header/header_button/Shopping_cart.png'
 									alt='Shopping cart'
 								/>
 							</a>
 						</span>
 						<a href='#' className='w-6 h-6'>
-							<img src='moon/public/header/header_button/User.png' alt='User' />
+							<img src='header/header_button/User.png' alt='User' />
 						</a>
 					</div>
 				</div>
